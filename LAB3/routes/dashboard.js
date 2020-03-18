80ba0cff78157df275d991a1fbf5c5f3d853d633
Pattanan Laohawiroje <phattanan1999@gmail.com>
2020-03-18 17:04:31 +0700

    upload LAB3

---
+++ LAB3/routes/dashboard.js
@@ -0,0 +1,16 @@
+const express = require('express') 
+const router = express.Router() 
+
+router.route('/')     
+    .get((req, res, next) => {
+        res.locals.pageData = {   
+            title:'Dashboard Page'         
+        }                 
+        res.render('pages/dashboard')
+    }) 
+router.route('/logout')     
+    .get((req, res, next) => {
+        res.redirect('/login')         
+    })     
+
+    module.exports = router 
